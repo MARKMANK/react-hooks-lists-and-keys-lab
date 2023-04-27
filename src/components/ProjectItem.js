80f6dde40098394056db2dcb46323cb712b0d87c
kevinmarkman <kevin.w.markman@gmail.com>
2023-04-27 11:15:28 -0500

    Adds map methods to complete lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+  const technologiesMapped = technologies.map((techItem)=>{
+    return <span key={techItem}> {techItem} </span>
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        
+        {
+        /* render a <span> for each technology in the technologies array */
+        technologiesMapped
+        }
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,19 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const projectsMapped = projects.map((entry)=>{
+    console.log(entry)
+    return <ProjectItem key={entry.id} name={entry.name} about={entry.about} technologies={entry.technologies}/>
+  })
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{
+      /* render ProjectItem components here */
+      projectsMapped
+      }</div>
     </div>
   );
 }
